b5ba2da8ed3948b02475c7ca932d975503ebd422
Jose E <benavides@estudiantec.cr>
2014-06-24 21:13:26 -0600

    Sort class

---
+++ Class/Sort/Clase15/src/Clase15.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+
+/**
+ *
+ * @author Estudiante
+ */
+public class Clase15 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        int[]arreglo={50,26,70,9,15};
+        int[]arregloOrdenado;
+        ordenamientoBurbuja ob=new ordenamientoBurbuja();
+        arregloOrdenado=ob.OrdenamientoB(arreglo, "A");
+        
+        for (int i = 0; i < arreglo.length; i++) {
+            System.out.println(arregloOrdenado[i]);
+            
+        }
+        
+        
+    }
+    
+}
---
+++ Class/Sort/Clase15/src/ordenamientoBurbuja.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Estudiante
+ */
+public class ordenamientoBurbuja {
+    public int []OrdenamientoB(int[] arreglo, String ordenamiento){
+    int indice=0;
+    int indice2=0;
+    int aux=0;
+    if(ordenamiento.equals("A")){
+        for (indice = 0; indice < arreglo.length-1; indice++) {
+            for (indice2 = 0; indice2 < arreglo.length-indice-1; indice2++) {
+                if(arreglo[indice2+1]<arreglo[indice2]){
+                    aux=arreglo[indice2+1];
+                    arreglo[indice2+1]=arreglo[indice2];
+                    arreglo[indice2]=aux;
+                }
+                
+            }
+            
+        }return arreglo;
+    
+
+}else{
+    }return arreglo;
+    }
+}
