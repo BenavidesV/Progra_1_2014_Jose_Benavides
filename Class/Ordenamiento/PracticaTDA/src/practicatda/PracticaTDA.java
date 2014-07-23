c911af160e5fe4d6dc453634de107b1f0aba297e
Jose E <benavides@estudiantec.cr>
2014-07-22 21:01:41 -0600

    updated 9.01pm

--- Class/Ordenamiento/PracticaTDA/src/practicatda/PracticaTDA.java
+++ Class/Ordenamiento/PracticaTDA/src/practicatda/PracticaTDA.java
@@ -18,23 +18,76 @@ public class PracticaTDA {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        int n = 0;
+        int ejercicio = 0,n=0;
+        char continuar = ' ';
+        boolean validar = true;
+
         Scanner teclado = new Scanner(System.in);
-        clsLista oLista = new clsLista();
-        System.out.println("Digite un número que será el nodo de la lista");
-        n = teclado.nextInt();
-        while (n != 0) {
-            oLista.Insertar(n);
-            System.out.println("Digite un número que será el nodo de la lista");
-            n = teclado.nextInt();
-        }
-        oLista.cantidad();
-        System.out.println("Digite un número que será eliminado de la lista");
-            n = teclado.nextInt();
-        oLista.Eliminar(n);
-        oLista.Imprimir();
-        
-        
+
+        do {
+            System.out.println("Digite el número del ejercicio que quiere ver");
+            ejercicio = Integer.parseInt(teclado.nextLine());
+            clsLista oLista = new clsLista();
+
+            switch (ejercicio) {
+                case 1:
+                    
+                    System.out.println("Digite un número que será el nodo de la lista");
+                    n = teclado.nextInt();
+                    while (n != 0) {
+                        oLista.Insertar(n);
+                        System.out.println("Digite un número que será el nodo de la lista");
+                        n = teclado.nextInt();
+                    }
+                    
+                    
+//        System.out.println("Digite un número que será eliminado de la lista");
+//            n = teclado.nextInt();
+//        oLista.Eliminar(n);
+                    oLista.Imprimir();
+
+                    break;
+
+                case 2:
+                    System.out.println("2. La lista tiene " + oLista.cantidad() + " elementos");
+                    break;
+
+                case 3:
+                    System.out.println("3. La suma del cuadrado de los "
+                            + "elementos es "+ oLista.SumatoriaCadrados());
+                    break;
+                case 4:
+                    System.out.println("Digite un número que será el nodo de la lista ");
+                    n = teclado.nextInt();
+                    while (n != 0) {
+                        oLista.Insertar(n);
+                        System.out.println("Digite un número que será el nodo de la lista");
+                        n = teclado.nextInt();
+                    }
+                    System.out.println("Digite un número que será el nodo de la lista 2 ");
+                    n = teclado.nextInt();
+                    while (n != 0) {
+                        oLista.InsertarOtra(n);
+                        System.out.println("Digite un número que será el nodo de la lista");
+                        n = teclado.nextInt();
+                    }
+                    System.out.println("Concatenadas "+"\n");
+                    oLista.Imprimir();
+                    break;
+
+            }
+
+            System.out.println("Desea continuar con otra operación S/N ");
+            teclado = new Scanner(System.in);
+            continuar = teclado.nextLine().charAt(0);
+            if ((continuar == 'S') || (continuar == 's')) {
+                validar = true;
+            } else {
+                validar = false;
+            }
+
+        } while (validar);
     }
+    
 
 }
--- Class/Ordenamiento/PracticaTDA/src/practicatda/clsLista.java
+++ Class/Ordenamiento/PracticaTDA/src/practicatda/clsLista.java
@@ -12,10 +12,12 @@ package practicatda;
 public class clsLista {
 
     private NodoDeLista raiz;
+    private NodoDeLista raiz2da;
     int[] vector1 = new int[cantidad()];
 
     public clsLista() {
         raiz = null;
+        raiz2da = null;
 
     }
 
@@ -36,15 +38,41 @@ public class clsLista {
         }
     }
 
+    public void InsertarOtra(int dato) {
+        NodoDeLista nuevo;
+        nuevo = new NodoDeLista();
+        nuevo.setDato(dato);
+        if (raiz2da == null) {
+            nuevo.setSiguiente(null);
+            raiz2da = nuevo;
+        } else {
+            NodoDeLista auxiliar = raiz2da;
+            while (auxiliar.getSiguiente() != null) {
+                auxiliar = auxiliar.getSiguiente();
+
+            }
+            auxiliar.setSiguiente(nuevo);
+        }
+    }
+
     public void Imprimir() {
         NodoDeLista recorrido = raiz;
-        System.out.println("Listado de todos los elementos ");
+        System.out.println("Listado de los elementos de la lista 1 ");
         while (recorrido != null) {
             System.out.println(recorrido.getDato());
             recorrido = recorrido.getSiguiente();
         }
         System.out.println();
+
+        NodoDeLista recorrido2do = raiz2da;
+        System.out.println("Listado de todos los elementos lista 2 ");
+        while (recorrido2do != null) {
+            System.out.println(recorrido2do.getDato());
+            recorrido2do = recorrido2do.getSiguiente();
+        }
+        System.out.println();
     }
+// Este es el ejercicio 2
 
     public int cantidad() {
         NodoDeLista recorrido = raiz;
@@ -54,6 +82,17 @@ public class clsLista {
             recorrido = recorrido.getSiguiente();
         }
         return cont;
+    }
+// Este método retorna la sumatoria del cuadrado de los elementos de la lista
+
+    public double SumatoriaCadrados() {
+        NodoDeLista recorrido = raiz;
+        double cuadrado = 0, sumatoria = 0;
+        while (recorrido != null) {
+            cuadrado = recorrido.getDato() * recorrido.getDato();
+            sumatoria += cuadrado;
+        }
+        return sumatoria;
     }
 
     public void Eliminar(int dato) {
@@ -67,5 +106,17 @@ public class clsLista {
             }
             recorrido = recorrido.getSiguiente();
         }
+    }
+
+    public NodoDeLista concatenar() {
+        NodoDeLista recorrido = raiz;
+        while (recorrido != null) {
+
+            if (recorrido.getSiguiente() == null) {
+                recorrido.setSiguiente(raiz2da);
+            }
+            recorrido = recorrido.getSiguiente();
+        }
+        return recorrido;
     }
 }
